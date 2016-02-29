ccee58b35661509d4d69583b473af2ebf0b88488
Colin Stevens <colin@colinjstevens.com>
2016-02-28 21:01:17 -0500

    Completely rewrote build script

--- build.py
+++ build.py
@@ -1,44 +1,61 @@
-from bs4 import BeautifulSoup
-try:
-    from termcolor import colored
-except ImportError:
-    vprint("Failed to import coloring library. Defaulting to boring terminal output.")
+import re
+import tempfile
 
-HTML_PARSER = "html5lib"
-indexFile = open("index.html")
-tabFiles = {
-    'about' : open("about.html"),
-    'cv' : open("cv.html"),
-    'contact' : open("contact.html"),
-}
-tabData = {}
+OUTFILE = open("index.html", 'w')
+BASEFILE = open("index_base.html")
 
-#TODO
-#   * Make backup of index file before creation
+def vprint(s, indentNum=0, **args):
+    '''Print wrapper, allows argument based indenting.'''
+    print("{0}{1}".format(("\t"*indentNum), s), **args)
 
-def vprint(s, **args):
-    '''Wrapper for print() in case I decide to add fancy debug prints later.'''
-    print(s, **args)
+def extractBody(f):
+    '''Creates a string array of the lines in a file `f` between lines containing "<body>",
+    and "</body>".'''
+    output = []
+    read = False
+    for l in f.readlines():
+        if "<body>" in l:
+            read = True
+            continue # Avoid appending "<body>" to output
+        elif "</body>" in l:
+            read = False
+        if read:
+            output.append(l)
+    return output
 
-def makeSoups():
-    vprint("Loading source files...")
+def buildIndex():
+    '''Matches lines in `BASEFILE` against a regex match, attempts to open files based on
+    matches and write their contents into a temporary file. Returns a temporary file
+    handler.'''
+    tmp = tempfile.TemporaryFile(mode='r+', encoding='UTF-8')
+    vprint("Scanning `{0}` for build comments...".format(BASEFILE.name), 1)
+    for line in BASEFILE.readlines():
+        result = re.match('\s*<!--##BUILD:(\s)?(?P<filePath>.*)##-->', line)
+        if result:
+            filePath = result.groupdict()['filePath']
+            vprint("* Found build comment. Inserting body content from `{0}`... ".format(filePath), 2, end="")
+            try:
+                sourceFile = open(filePath)
+            except FileNotFoundError as e:
+                vprint("failed ({0})".format(e))
+                continue
+            for l in extractBody(sourceFile):
+                tmp.write(l)
+            sourceFile.close()
+            vprint("done")
+        else:
+            tmp.write(line)
+    tmp.seek(0)
+    vprint("Index concatenated.", 1)
+    return tmp
 
-    vprint("\t* loading index... ", end="")
-    indexSoup = BeautifulSoup(indexFile, HTML_PARSER)
-    vprint(colored("done.", 'green'))
-
-    for key in tabFiles.keys():
-        vprint("\t* loading {0}... ".format(tabFiles[key].name), end="")
-        tabData[key] = BeautifulSoup(tabFiles[key], HTML_PARSER)
-        vprint(colored("done.", 'green'))
-
-def build():
-    makeSoups()
 
 if __name__ == "__main__":
-    vprint("Beginning build\n---------------")
-    build()
-    vprint("---------------\nBuild complete.") #TODO: Check for build errors
-print(tabData['about'].body)
-print(tabData['cv'].body)
-print(tabData['contact'].body)
+    vprint("Building index...")
+    tmp = buildIndex()
+    OUTFILE.write(tmp.read())
+    # Cleanup
+    tmp.close()
+    BASEFILE.close()
+    OUTFILE.close()
+    vprint("Build complete.") #TODO: Check for build errors
